e1bf8862df69f9c379f87fa427d3d36e0c6cce06
欧启燊 <437236032@qq.com>
2025-04-20 11:54:57 +0800

    feat(user&channel) add user_create&channel_nft_create/mint

---
+++ anchor/programs/zoku/src/instructions/mod.rs
@@ -0,0 +1,7 @@
+pub mod channel_nft_create;
+pub mod channel_nft_mint;
+pub mod user;
+
+pub use channel_nft_create::*;
+pub use channel_nft_mint::*;
+pub use user::*;
---
+++ anchor/programs/zoku/src/states/channel.rs
@@ -0,0 +1,55 @@
+use anchor_lang::prelude::*;
+use anchor_spl::metadata::mpl_token_metadata;
+
+#[account]
+#[derive(InitSpace)]
+pub struct ChannelInfo {
+    pub mint_account: Pubkey,
+    pub mint_num: u64,
+    pub is_enabled: bool,
+    pub like: u64,
+    pub num_of_subscribers: u64,
+    pub num_of_audios: u64,
+    pub created_at: i64,
+    pub type_of_cost: TypeOfCost,
+
+    #[max_len(50)]
+    pub description: String,
+
+    #[max_len(10)]
+    pub creators: Vec<Creator>,
+
+    #[max_len(100)]
+    pub ipfs_hash: String,
+}
+
+#[derive(InitSpace, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct Creator {
+    pub address: Pubkey,
+    pub share: u8,
+    pub verified: bool,
+}
+#[derive(InitSpace, Clone, AnchorSerialize, AnchorDeserialize)]
+pub enum TypeOfCost {
+    Free = 0,
+    Paid = 1,
+}
+
+impl ChannelInfo {
+    pub const SEED_PREFIX: &'static str = "channelInfo_v1";
+
+    pub fn convert_to_metadata_creators(
+        creators: Vec<Creator>,
+    ) -> Option<Vec<mpl_token_metadata::types::Creator>> {
+        Some(
+            creators
+                .into_iter()
+                .map(|creator| mpl_token_metadata::types::Creator {
+                    address: creator.address,
+                    verified: creator.verified,
+                    share: creator.share,
+                })
+                .collect(),
+        )
+    }
+}
