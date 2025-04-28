681ec8b91a71cf0b458da8aaa99b63cf27c004ef
Shushunie <lcr34561@gmail.com>
2025-04-28 20:36:05 +0800

    chore: initial setup for server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ScheduleModule } from '@nestjs/schedule';
-import { ConfigModule } from '@nestjs/config';
-import { TasksModule } from './tasks/tasks.module';
-import { ProgramModule } from './program/program.module';
 
 @Module({
-  imports: [
-    ScheduleModule.forRoot(),
-    ConfigModule.forRoot({
-      envFilePath: ['.env.pro', '.env.local', '.env'],
-      isGlobal: true,
-    }),
-    TasksModule,
-    ProgramModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('pozoko server')
-    .setDescription('The pozoko API description')
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
     .setVersion('1.0')
-    .addTag('pozoko')
+    .addTag('cats')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
